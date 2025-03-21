61d25b8fdd7816a5aebe9d0b5494bb681cc108f1
gokhanymn <gokhanyman@gmail.com>
2025-03-21 12:52:11 +0300

    Project 2 nin eklenmesi

---
+++ NetCoreAI.Project2_ApiConsumeUI/Dtos/ResultCustomerDto.cs
@@ -0,0 +1,11 @@
+﻿namespace NetCoreAI.Project2_ApiConsumeUI.Dtos
+{
+    public class ResultCustomerDto
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerSurname { get; set; }
+        public decimal CustomerBalance { get; set; }
+    }
+
+}
